5c8c57f248faa5f46ad70921322218237d1fd54d
Gabriela Rodrigues <ladyg432@gmail.com>
2021-09-06 20:01:17 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/gabrielarodrigues/cursomc/services/CategoriaService.java
+++ src/main/java/com/gabrielarodrigues/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.gabrielarodrigues.cursomc.services;
 
 import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);		
+		return obj.orElseThrow(() -> new com.gabrielarodrigues.cursomc.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
 	}
 	
-}
+
